07f88e437779bc151cf0959eb9d6f048e4b3b8f9
pedroaz <pedro.azvm@gmail.com>
2020-10-10 09:38:19 -0300

    Starting to create area screen

---
+++ SevenWorlds.Unity/Assets/_Game/UI/PlayerActionsButtons/ShowPlayerActionsButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ShowPlayerActionsButton : GameButton
+{
+    public override Task OnClick()
+    {
+        return base.OnClick();
+    }
+}
