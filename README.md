5f76ae569b130171944c0556ffae59d2e61d52fb
pedroaz <pedro.azvm@gmail.com>
2020-10-11 12:00:36 -0300

    Added READ.README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+#Seven Worlds
+
+## What is Seven Worlds?
+Seven Worlds is an Open Source rogue like MMORPG
+
+## Techinical Components:
+*Unity 2020.1.3f1
--- SevenWorlds.Server/src/SevenWorlds.Shared/UnityLog/LOG.cs
+++ SevenWorlds.Server/src/SevenWorlds.Shared/UnityLog/LOG.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 
 namespace SevenWorlds.Shared.UnityLog
 {
     public static class LOG
     {
-
+        public static void Log(object obj)
+        {
+            Debug.Log(obj.ToString());
+        }
     }
 }
