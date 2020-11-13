f46cba96a26ac39bac30db15d47fc446b9a0912a
pedroaz <pedro.azvm@gmail.com>
2020-11-12 21:14:58 -0300

    Adding unit sketches

---
+++ SevenWorlds.Unity/Assets/_Game/BattleGround/UnitManagers/EquipmentItem.cs
@@ -0,0 +1,21 @@
+﻿using SevenWorlds.Shared.Data.Gameplay;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum AttackAnimation
+{
+    None,
+    Swing
+}
+
+[CreateAssetMenu(menuName = "SevenWorlds/EquipmentItem")]
+public class EquipmentItem : ScriptableObject
+{
+    public EquipmentId equipmentId;
+    public GameObject prefab;
+    public Vector2 parentPosition;
+    public Vector2 equipmentPosition;
+    public AttackAnimation attackAnimation; 
+}
